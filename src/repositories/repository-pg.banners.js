aa3b40e93e76a8b09b8c53515d5ac253c5bfdc37
Eduardo Abreu <eduardoabreu69@gmail.com>
2026-01-04 01:44:09 -0300

    edit banner tipo

--- src/repositories/repository-pg.banners.js
+++ src/repositories/repository-pg.banners.js
@@ -5,20 +5,19 @@ import db from "../database/postgresql.js";
  */
 async function PegarBanner(id_banner) {
   const sql = `
-    SELECT id_banner, banner
+    SELECT id_banner, banner, tipo
       FROM banners
      WHERE id_banner = $1
   `;
-
   const { rows } = await db.query(sql, [id_banner]);
   return rows[0] || null;
 }
 
-/**
- * Listar todos os banners
- */
 async function ListarBanners() {
-  const sql = `SELECT * FROM banners`;
+  const sql = `
+    SELECT id_banner, banner, tipo
+      FROM banners
+  `;
   const { rows } = await db.query(sql);
   return rows;
 }
@@ -30,7 +29,7 @@ async function InserirBanner(fotoUrl) {
   const sql = `
     INSERT INTO banners (banner)
     VALUES ($1)
-    RETURNING id_banner
+    RETURNING id_banner, banner, tipo
   `;
 
   const { rows } = await db.query(sql, [fotoUrl]);
@@ -45,7 +44,7 @@ async function EditarBanner({ id_banner, foto }) {
     UPDATE banners
        SET banner = COALESCE($1, banner)
      WHERE id_banner = $2
-     RETURNING id_banner, banner
+     RETURNING id_banner, banner, tipo
   `;
 
   const { rows } = await db.query(sql, [foto, id_banner]);
