d5744bdda5c4828d5c25c054391be8ff404455bd
Eduardo Abreu <eduardoabreu69@gmail.com>
2026-01-04 14:02:56 -0300

    editando index.js

--- src/index.js
+++ src/index.js
@@ -7,26 +7,27 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
-// para conseguir usar __dirname com ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// middlewares
 app.use(express.json());
-app.use(cors());
 
-// serve a pasta uploads como pública para imgs
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "HEAD", "OPTIONS"],
+  allowedHeaders: ["Range", "Content-Type", "Authorization"],
+  exposedHeaders: ["Content-Range", "Accept-Ranges", "Content-Length"]
+}));
+
+// imagens
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
-// serve a pasta uploads_videos como pública para videos
-app.use("/uploads_videos", express.static(path.resolve("uploads_videos")));
 
+// vídeos (IMPORTANTE)
+app.use("/uploads_videos", express.static(path.join(__dirname, "../uploads_videos")));
 
-// rotas
 app.use(router);
 
-// iniciar servidor
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
