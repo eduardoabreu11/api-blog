7c3bca9fdfca12801b0932da977dcc2a85598158
Eduardo Abreu <eduardoabreu69@gmail.com>
2026-01-15 21:39:45 -0300

    fix: cloudinary default export

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,16 +1,10 @@
-import cloudinary from "./cloudinary.js";
-import streamifier from "streamifier";
+import { v2 as cloudinary } from "cloudinary";
+import "dotenv/config";
 
-export function uploadToCloudinary(file, folder) {
-  return new Promise((resolve, reject) => {
-    const uploadStream = cloudinary.uploader.upload_stream(
-      { folder },
-      (error, result) => {
-        if (error) return reject(error);
-        resolve(result);
-      }
-    );
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-    streamifier.createReadStream(file.buffer).pipe(uploadStream);
-  });
-}
+export default cloudinary;
